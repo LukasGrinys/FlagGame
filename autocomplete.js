3244a4b79a99e7493dbd2ec54adf481b8fb17aec
Lukas <53226241+LukasGrinys@users.noreply.github.com>
2019-09-12 19:10:31 +0300

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -6,7 +6,7 @@
                     "Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic", 
                     "East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia",
                    "Eswatini","Ethiopia","Fiji","Finland", "France","Gabon","Gambia","Georgia","Germany","Ghana","Greece",
-                   "Grenada", "Guatemala","Guinea","Guinea-Bissau","Guyana","Guinea Bissau", "Guyana", "Haiti", "Honduras",
+                   "Grenada", "Guatemala","Guinea","Guinea-Bissau", "Guyana", "Haiti", "Honduras",
                    "Hungary","Iceland","India","Indonesia","Iran","Iraq"];
 
 function autocomplete(inp, arr) {
