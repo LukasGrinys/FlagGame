52da15d26b246c15aa8deb494d8f0618dc57bf61
Lukas <53226241+LukasGrinys@users.noreply.github.com>
2019-09-15 19:44:53 +0300

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -7,7 +7,8 @@
                     "East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia",
                    "Eswatini","Ethiopia","Fiji","Finland", "France","Gabon","Gambia","Georgia","Germany","Ghana","Greece",
                    "Grenada", "Guatemala","Guinea","Guinea-Bissau", "Guyana", "Haiti", "Honduras",
-                   "Hungary","Iceland","India","Indonesia","Iran","Iraq"];
+                   "Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Ivory Coast","Jamaica",
+                   "Japan","Jordan", "Kazakhstan", "Kenya", "Kiribati"];
 
 function autocomplete(inp, arr) {
   var currentFocus;
