fde7e5d54a6cbdddc1943af6629876bec8d48746
LukasGrinys <lukas.grinys@gmail.com>
2019-10-19 10:17:24 +0300

    Flag catalog completed!

--- autocomplete.js
+++ autocomplete.js
@@ -14,7 +14,11 @@
 "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan","Palau","Palestine", "Panama", "Papua New Guinea", "Paraguay",
 "Peru", "Philippines","Poland","Portugal","Qatar","Romania","Russia","Rwanda","Saint Kitts and Nevis","Saint Lucia",
 "Saint Vincent and the Grenadines", "Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia",
-"Seychelles","Sierra Leone", "Singapore","Slovakia","Slovenia"];
+"Seychelles","Sierra Leone", "Singapore","Slovakia","Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan",
+"Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Togo",
+"Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
+"United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia",
+"Zimbabwe"];
 
 function autocomplete(inp, arr) {
   var currentFocus;
